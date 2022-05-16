18c37243af667c30c4277b2bc580d5aa8b980275
CristianDavid <cristianescobarcdec@gmail.com>
2022-05-16 15:50:38 -0500

    iconos

--- src/components/elementos/SideBar.js
+++ src/components/elementos/SideBar.js
@@ -1,6 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Dropdown } from "react-bootstrap";
+import { HiPhotograph } from "react-icons/hi";
+import { VscFilePdf, VscSaveAs, VscSave, VscHome, VscServerProcess} from "react-icons/vsc";
+import { SiMicrosoftexcel } from "react-icons/si";
+import { BiChalkboard } from "react-icons/bi";
+import { TiExport } from "react-icons/ti";
+import { AiTwotoneAppstore } from "react-icons/ai";
 
 const SideBarGrafo = () => {
   return (
@@ -8,36 +14,36 @@ const SideBarGrafo = () => {
       <div className="card">
         <div className="container d-grid gap-1 card-body">
           <Link to="../home" className="btn btn-dark">
-            Home
+            <VscHome/> Home
           </Link>
           <Link to="" className="btn btn-dark">
-            Tablero
+            <BiChalkboard /> Tablero
           </Link>
           <Link to="aplicacion" className="btn btn-dark">
-            Aplicacion
+            <AiTwotoneAppstore/> Aplicacion
           </Link>
           <Link to="analizar" className="btn btn-dark">
-            Analizar
+            <VscServerProcess/> Analizar
           </Link>
 
-          <Dropdown className= "d-block">
+          <Dropdown className="d-block">
             <Dropdown.Toggle id="dropdown-autoclose-true" className="w-100">
-               Guardar 
+              <VscSave /> Guardar
             </Dropdown.Toggle>
             <Dropdown.Menu>
-              <Dropdown.Item href="#">Guardar</Dropdown.Item>
-              <Dropdown.Item href="#">Guardar Como</Dropdown.Item>
+              <Dropdown.Item href="#"><VscSave /> Guardar </Dropdown.Item>
+              <Dropdown.Item href="#"><VscSaveAs /> Guardar Como </Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
 
           <Dropdown>
             <Dropdown.Toggle id="dropdown-autoclose-true" className="w-100">
-               Exportar 
+            <TiExport/> Exportar
             </Dropdown.Toggle>
             <Dropdown.Menu>
-              <Dropdown.Item href="#">Imegen</Dropdown.Item>
-              <Dropdown.Item href="#">Pdf</Dropdown.Item>
-              <Dropdown.Item href="#">Excel</Dropdown.Item>
+              <Dropdown.Item href="#"><HiPhotograph /> Imegen </Dropdown.Item>
+              <Dropdown.Item href="#"><VscFilePdf /> Pdf </Dropdown.Item>
+              <Dropdown.Item href="#"><SiMicrosoftexcel /> Excel </Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
         </div>
